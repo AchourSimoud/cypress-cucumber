d28757d009f54190141b3dd723283b427e4bdd87
Tarek HALLALEL <TarekHALLALEL@gmail.com>
2025-02-23 23:34:45 +0100

    feature/ajout test AffichagedetailleProduit

--- cypress/features/affichageProduitDetailles.feature
+++ cypress/features/affichageProduitDetailles.feature
@@ -1,28 +1,26 @@
 Feature: Affichage du detaille produits
-  Scenario: Vérification du detailles des produits
+
+  Background:
     Given je suis sur la page produits
     When je clique sur un produit "Sauce Labs Backpack"
+
+  Scenario: Vérification du detailles des produits
     Then je retrouve le produit avec sa description son prix son image dans le detaille prodiot
 
-#   Scenario: Vérification du button remove
-#     Given L'utilisateur est sur la page d'accueil
-#     When  je clique sur un produit 
-#     Then La  detaille produits s'affiche
-#     And  je clique sur remove 
-#     And  le nombre du cart badge decrement 
-#     And le button remove devient add 
+  Scenario: Vérification du button add card 
+    Then je retrouve le produit avec sa description son prix son image dans le detaille prodiot
+    And je recupere la valeur actuel de cart badge
+    And  je clique sur add to cart
+    And  le nombre du cart badge increment 
+    And le button add cart  devient remove  
+    And je recupere la valeur actuel de cart badge
+    And je clique sur le button remove
+    And  le nombre du cart badge decrement 
+    And le button remove  devient add ton cart 
 
-#   Scenario: Vérification du button add card 
-#     Given L'utilisateur est sur la page d'accueil
-#     When  je clique sur un produit 
-#     Then La  detaille produits s'affiche
-#     And  je clique sur add to cart
-#     And  le nombre du cart badge increment 
-#     And le button add cart  devient remove  
 
-#   Scenario: Vérification du button back to product
-#     Given L'utilisateur est sur la page d'accueil
-#     When je clique sur un produit 
-#     Then La  detaille des produits s'affiche
-#     And je clique sur back to product 
-#     And la page d'accueil s'affiche
+  Scenario: Vérification du button retour au inventaire  
+    Then je retrouve le produit avec sa description son prix son image dans le detaille prodiot
+    And je clique sur back to product
+    And la page daccueil saffiche  
+
--- cypress/steps/affichageProduitDetaille.steps.js
+++ cypress/steps/affichageProduitDetaille.steps.js
@@ -3,10 +3,6 @@
 import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
 import productPage from "../pages/product.page";
 import detaillePage from "../pages/detaille.page";
-
-
-
-
 Then('je retrouve le produit avec sa description son prix son image dans le detaille prodiot', () => {
 
     let expectedPrice = Cypress.env("addedProductPrice");
@@ -30,11 +26,9 @@ Then('je retrouve le produit avec sa description son prix son image dans le deta
         .then((imageSrc) => {
             cy.wrap(imageSrc).should("equal", expectedImage);
         });
-
 })
-
 When('je clique sur un produit {string}', (produit) => {
-    
+
     productPage.getProductDescription(produit).then((desc) => {
         Cypress.env("addedProductDesc", desc);
     });
@@ -44,12 +38,56 @@ When('je clique sur un produit {string}', (produit) => {
     productPage.getProductImage(produit).then((image) => {
         Cypress.env("addedProductimage", image);
     }
-    
-
-);
-    productPage.cliquerSurProduit(produit);})
+    );
+    productPage.cliquerSurProduit(produit);
+})
 
 Given('je suis sur la page produits&#39;', () => {
     cy.visit("https://www.saucedemo.com/");
     loginPage.login("standard_user", "secret_sauce");
+})
+
+Then('je clique sur add to cart', () => {
+    detaillePage.ajouterToCartButton()
+})
+
+Then('le button add cart  devient remove', () => {
+    detaillePage.elements.retirerDuPanierBouton().should('be.visible')
+})
+let initialCartCount = 0;
+Then('je recupere la valeur actuel de cart badge', () => {
+
+    productPage.elements.cartIcon().invoke('text').then((text) => {
+        initialCartCount = text ? parseInt(text, 10) : 0;
+        cy.log(`Nouveau nombre dans le cart badge: ${initialCartCount}`);
+    })
+})
+
+Then('le nombre du cart badge increment', () => {
+    productPage.elements.cartIcon().invoke('text').then((newText) => {
+        const newCartCount = parseInt(newText, 10);
+        cy.wrap(newCartCount).should('equal', initialCartCount + 1);
+    })
+})
+
+Then('je clique sur back to product', () => {
+    detaillePage.allerAuInventaire()
+})
+
+Then('la page daccueil saffiche', () => {
+    cy.url().should("include", "inventory")
+
+})
+
+Then('je clique sur le button remove', () => {
+    detaillePage.retirerProduitDuPanier()
+})
+Then('le button remove  devient add ton cart', () => {
+    detaillePage.elements.addToCartButton().should('be.visible')
+})
+Then('le nombre du cart badge decrement', () => {
+    productPage.elements.cartIcon().invoke('text').then((newText) => {
+        const newCartCount = newText ? parseInt(newText, 10) : 0;
+        cy.wrap(newCartCount).should('equal', initialCartCount - 1);
+    })
 })
