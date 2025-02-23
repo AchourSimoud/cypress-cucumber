506937a73afe1372f51b4789ed0ec2a4d14e38fc
Arnaud <ngabgnaarnaud2@gmail.com>
2025-02-23 13:31:08 +0100

    update tri

--- cypress/features/tri_az_za.feature
+++ cypress/features/tri_az_za.feature
@@ -1,6 +1,6 @@
 Feature: Tri des produits par nom
   Scenario: Trier les produits de A a Z
-    Given L utilisateur est sur la page d'accueil
+    Given L utilisateur est sur la page d accueil
     When Il sélectionne "Name (A to Z)" dans le menu de tri
     Then Les produits sont tries dans l ordre alphabétique
 
---
+++ cypress/steps/tri_az_za.steps.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+
+Given('L utilisateur est sur la page d accueil', () => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+Then('Les produits sont tries dans l ordre alphabétique', () => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+When('Il sélectionne {string} dans le menu de tri', (s) => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+Then('Les produits sont tries dans l ordre alphabetique inverse', () => {
+  // Write code here that turns the phrase above into concrete actions
+})
+
+When('Il selectionne {string} dans le menu de tri', (s) => {
+  // Write code here that turns the phrase above into concrete actions
+})
